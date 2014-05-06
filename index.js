24fb73520f1e2b2170c8ef3c929791d89ef41f03
Jen Wachter <jenleighkelly@gmail.com>
2014-05-06 12:23:29 -0400

    Fixed wptheme and wpplugin tasks.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var _ = require("underscore");
 var argv = require("minimist")(process.argv.slice(2));
 
-var tasks = {
+var getTask = {
   php: require("./lib/tasks-php"),
   js: require("./lib/tasks-js"),
   css: require("./lib/tasks-css"),
@@ -13,36 +13,48 @@ var tasks = {
 var taskTypes = function () {};
 
 taskTypes.prototype.php = function () {
-  return _.extend(tasks.php(), tasks.release());
+  return _.extend(getTask.php(), getTask.release());
 };
 
 taskTypes.prototype.js = function () {
-  return _.extend(tasks.js(), tasks.release());
+  return _.extend(getTask.js(), getTask.release());
 };
 
 taskTypes.prototype.css = function () {
-  return _.extend(tasks.css(), tasks.release());
+  return _.extend(getTask.css(), getTask.release());
 };
 
 taskTypes.prototype.images = function () {
-  return _.extend(tasks.images(), tasks.release());
+  return _.extend(getTask.images(), getTask.release());
 };
 
 taskTypes.prototype.wptheme = function () {
-  var tasks = _.extend(tasks.js(), tasks.css(), tasks.php());
+  
+  gulp.tasks = _.extend(getTask.js(), getTask.css(), getTask.images());
 
-  tasks.watch = function () {
+  gulp.task("watch", ["compile:js", "compile:css", "move:images"], function () {
+    gulp.watch(["./src/js/**/*.js", "!./src/js/vendor/**/*.js"], ["compile:js"]);
+    gulp.watch(["./src/css/**/*.scss", "!./src/css/vendor/**/*.scss"], ["compile:css"]);
+    gulp.watch(["./src/images/**/*"], ["move:images"]);
+  });
 
-    // update js
-    // update css
-    // run php tests
-
-  };
+  return gulp.tasks;
 
 };
 
-taskTypes.prototype.release = function () {
-  return tasks.release();
+taskTypes.prototype.wpplugin = function () {
+
+  gulp.tasks = _.extend(getTask.js(), getTask.css(), getTask.images(), getTask.php());
+
+  gulp.task("watch", ["compile:js", "compile:css", "move:images"], function () {
+    gulp.watch(["./src/js/**/*.js", "!./src/js/vendor/**/*.js"], ["compile:js"]);
+    gulp.watch(["./src/css/**/*.scss", "!./src/css/vendor/**/*.scss"], ["compile:css"]);
+    gulp.watch(["./src/images/**/*"], ["move:images"]);
+    gulp.watch(["./src/**/*.php", "./tests/**/*.php"], ["phpunit"]);
+  });
+
+  return gulp.tasks;
+
 };
 
 module.exports = new taskTypes();
